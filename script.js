9b3bc092020de2dd3a4d7ed3668ceefd6234c83d
Ilya <iilyax11@gmail.com>
2023-06-04 20:58:14 +0300

    game logic imlemented

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.question').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', () => {
-  const inputValue = document.querySelector('.number-input').value;
+  const inputValue = Number(document.querySelector('.number-input').value);
+
+  if (!inputValue) {
+    document.querySelector('.guess-message').textContent = 'Input Number!';
+  } else if (inputValue > secretNumber && inputValue <= 20) {
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent = 'Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent = 'GAME OVER';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (inputValue === secretNumber) {
+    if (score > 0) {
+      document.querySelector('.guess-message').textContent = 'You are right!!!';
+    }
+  } else if (inputValue < secretNumber && inputValue > 0) {
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent = 'Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent = 'GAME OVER';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (inputValue < 0 || inputValue > 20) {
+    document.querySelector('.guess-message').textContent =
+      'Not between 1 to 20';
+  }
 });
